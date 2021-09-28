6eaaadecde4d91f4b6c4a2d24fa7f365d9971887
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-09-28 18:03:49 +0530

    pages, components and shared ui elements added

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,84 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <base-badge v-for="area in areas"
+       :key="area"
+       :type="area"
+       :title="area"
+       ></base-badge>
+    </div>
+    <div class="actions">
+      <base-button mode="outline" link :to="coachContactLink">Contact</base-button>
+      <base-button link :to="coachDetailLink">View Details</base-button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      required: true,
+    },
+    areas: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    coachContactLink() {
+      // 2 ways to achieve this. router.path advantage is we can change router of contact in router file without touching this file code.
+      //   return `/coaches/${this.id}/contact`;
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+    coachDetailLink() {
+      return `${this.$route.path}/${this.id}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
