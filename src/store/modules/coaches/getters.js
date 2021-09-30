5316117d2136cd03af554de4da63e662562855c7
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-09-30 16:19:27 +0530

    register coach functionality added

--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -4,5 +4,10 @@ export default {
     },
     hasCoaches(state) {
         return !!state.coaches?.length;
+    },
+    isCoach(state, getters, rootState, rootGetters) {
+        const coaches = getters.coaches;
+        const userId = rootGetters.userId;
+        return coaches.some(coach => coach.id === userId);
     }
 };
