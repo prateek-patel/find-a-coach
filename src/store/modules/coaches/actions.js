10e5af13b6bb4cccefd6fe94f3490444d8fb9dc6
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-10-18 18:47:03 +0530

    sending PUT http request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,5 +1,6 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
             id: context.rootGetters.userId,
             firstName: data.first,
@@ -8,6 +9,22 @@ export default {
             hourlyRate: data.rate,
             areas: data.areas
         };
-        context.commit('registerCoach', coachData);
+
+        const response = await fetch(`https://vue-http-request-65e90-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            // error ...
+        }
+
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 };
