26f71b92434205c631e2d663823357cee112cc4a
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-10-21 18:27:25 +0530

    fetching coach data and rendering loading spinner

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,10 +5,13 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register">Register as a Coach</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+        <base-button v-if="!isCoach && !isLoading" link to="/register">Register as a Coach</base-button>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -32,6 +35,7 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
       return {
+        isLoading: false,
           activeFilters: {
               frontend: true,
               backend: true,
@@ -59,12 +63,20 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
+  },
+  created(){
+    this.loadCoaches();
   },
   methods: {
       setFilters(updatedFilters) {
           this.activeFilters = updatedFilters;
+      },
+      async loadCoaches() {
+        this.isLoading = true;
+        await this.$store.dispatch("coaches/loadCoaches");
+        this.isLoading = false;
       }
   }
 };
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -2,7 +2,7 @@ export default {
     async registerCoach(context, data) {
         const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
+            // id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
@@ -26,5 +26,28 @@ export default {
             ...coachData,
             id: userId
         });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(`https://vue-http-request-65e90-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+        );
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // ...
+        }
+
+        const coaches = [];
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+        context.commit('setCoaches', coaches);
     }
 };
