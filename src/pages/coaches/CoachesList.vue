c112ccb9fdcb0d1f1676000aa2a404885e19beff
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-11-09 16:06:13 +0530

    adding http error handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,7 @@
 <template>
+<base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+<p>{{error}}</p>
+</base-dialog>
   <section>
       <coach-filter @change-filter="setFilters"></coach-filter>
   </section>
@@ -36,11 +39,12 @@ export default {
   data() {
       return {
         isLoading: false,
-          activeFilters: {
-              frontend: true,
-              backend: true,
-              career: true
-          }
+        error: null,
+        activeFilters: {
+            frontend: true,
+            backend: true,
+            career: true
+        }
       };
   },
   computed: {
@@ -75,8 +79,15 @@ export default {
       },
       async loadCoaches() {
         this.isLoading = true;
-        await this.$store.dispatch("coaches/loadCoaches");
+        try {
+          await this.$store.dispatch("coaches/loadCoaches");
+        } catch (error) {
+          this.error = error.message || 'Something went wrong';
+        }
         this.isLoading = false;
+      },
+      handleError() {
+        this.error = null;
       }
   }
 };
